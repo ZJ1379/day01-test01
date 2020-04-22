12eea1891b2626303fe3a4ed9d7462adc895d2ef
ZJ1379 <64082255+ZJ1379@users.noreply.github.com>
2020-04-22 11:37:34 +0800

    Update Person.java

--- src/main/java/cn/jiyun/Person.java
+++ src/main/java/cn/jiyun/Person.java
@@ -5,5 +5,6 @@ public class Person {
     public void add(){
         System.out.println("今天的作业正在完成！！！");
         System.out.println("进行代码的修改并进行上传！！");
+        System.out.println("在远程仓库中修改代码并在idea中进行同步！！");
     }
 }
